f8893cf9020fb8d788f4f34115ce041c693734ad
nlau77 <nlau777@gmail.com>
2017-05-01 14:00:21 -0400

    foundation installed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groceries
-  root("groceries#index")
+  root("static_pages#index")
+
+  resources :users, only: [:show]
 end
