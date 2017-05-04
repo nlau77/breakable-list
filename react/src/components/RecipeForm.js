90744efcf0a594fe8e050f631faba8aeb6104880
nlau77 <nlau777@gmail.com>
2017-05-04 14:04:29 -0400

    handle change added for recipe: name, instructions, ingredients

--- react/src/components/RecipeForm.js
+++ react/src/components/RecipeForm.js
@@ -3,15 +3,21 @@ import { Link } from 'react-router';
 
 const RecipeForm = props => {
   return (
-    <form>
+    <form >
       <label>Name:</label>
-      <input type='text'/>
+      <input type='text' value={props.name} onChange={props.handleNameChange}/>
 
       <label>Instructions:</label>
-      <input type='text'/>
+      <input type='text' value={props.instructions} onChange={props.handleInstructionsChange}/>
 
       <label>Ingredients: <span>(please seperate ingredients with a comma)</span>: </label>
-      <input type='text'/>
+      <input type='text' value={props.ingredients} onChange={props.handleIngredientsChange}/>
+
+      <div className="rows">
+        <div className="offset-small-6 columns">
+          <input type='submit' className='submit-button' />
+        </div>
+      </div>
     </form>
   )
 }
--- react/src/containers/CreateRecipe.js
+++ react/src/containers/CreateRecipe.js
@@ -10,13 +10,45 @@ class CreateRecipe extends Component {
       instructions: '',
       ingredients: ''
     }
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleInstructionsChange = this.handleInstructionsChange.bind(this)
+    this.handleIngredientsChange = this.handleIngredientsChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleNameChange(event){
+      let newName = event.target.value
+      this.setState({ name: newName})
+  }
+
+  handleInstructionsChange(event){
+      let newInstructions = event.target.value
+      this.setState({ instructions: newInstructions})
+  }
+
+  handleIngredientsChange(event){
+      let newIngredients = event.target.value
+      this.setState({ ingredients: newIngredients})
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+
   }
 
   render(){
     return(
       <div className="rows">
+        <h3 className="text-center">Submit A Recipe!</h3>
         <div className="columns small-8 small-centered">
-          <RecipeForm />
+          <RecipeForm
+          handleNameChange={this.handleNameChange}
+          handleInstructionsChange={this.handleInstructionsChange}
+          handleIngredientsChange={this.handleIngredientsChange}
+          name={this.state.name}
+          instructions={this.state.instructions}
+          ingredients={this.state.ingredients}
+          />
         </div>
       </div>
     )
