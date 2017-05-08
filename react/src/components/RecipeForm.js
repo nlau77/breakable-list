d9e13dfd9ace4f18bceb1e67d516cf43b2660332
nlau77 <nlau777@gmail.com>
2017-05-08 14:07:39 -0400

    downsized create recipe form

--- react/src/components/RecipeForm.js
+++ react/src/components/RecipeForm.js
@@ -2,21 +2,20 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const RecipeForm = props => {
+  // <label>Instructions:</label>
+  // <input type='text' value={props.instructions} onChange={props.handleInstructionsChange}/>
+  //
+  // <label>Ingredients: </label>
+  // <input type='text' value={props.ingredients} onChange={props.handleIngredientsChange}/>
+  // <span>(please seperate ingredients with a comma, example cheese, mushrooms, onions)</span>
+
   return (
     <form onSubmit={props.handleSubmit}>
-      <label>Name:</label>
+      <label className="text-center">Give it a title:</label>
       <input type='text' value={props.name} onChange={props.handleNameChange}/>
-
-      <label>Instructions:</label>
-      <input type='text' value={props.instructions} onChange={props.handleInstructionsChange}/>
-
-      <label>Ingredients: </label>
-      <input type='text' value={props.ingredients} onChange={props.handleIngredientsChange}/>
-      <span>(please seperate ingredients with a comma, example cheese, mushrooms, onions)</span>
-
       <div className="rows">
-        <div className="offset-small-6 columns">
-          <input type='submit' className='submit-button' />
+        <div className="small-2 offset-small-6 offset-large-6 offset-medium-6 columns">
+          <input type='submit' className='submit-button' value="Start!"/>
         </div>
       </div>
     </form>
