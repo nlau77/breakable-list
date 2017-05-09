f378fa8cbdcace9fc1f5c9e27db7ac5dc8dbc489
nlau77 <nlau777@gmail.com>
2017-05-09 18:36:24 -0400

    moved add button for instructions

--- react/src/components/InstructionForm.js
+++ react/src/components/InstructionForm.js
@@ -6,7 +6,9 @@ const InstructionForm = (props) => {
     <form onSubmit={props.handleInstructionSubmit}>
       <label>Add step</label>
       <input type="text" value={props.newInstruction} onChange={props.handleStepChange}/>
-      <input type="submit" value="+"/>
+      <div className="small-offset-4">
+        <input className="button" type="submit" value="Add"/>
+      </div>
     </form>
   )
 }
